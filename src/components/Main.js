949f94a4788ec593a83b05f89a6883a14d64f6b4
TiagoAlmeidaLikeWaterCS <tiago.almeida@likewatercs.com>
2022-01-21 18:09:35 -0300

    Primeiro commit

---
+++ src/components/Main.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+
+// Form
+import { FaPlus } from 'react-icons/fa';
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+
+import './Main.css'
+
+export default class Main extends Component{
+    state = {
+        novaTarefa: '',
+        tarefas: [],
+        index: -1
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+
+      const { tarefas, index } = this.state;
+      let { novaTarefa } = this.state;
+      novaTarefa = novaTarefa.trim();
+
+      if(tarefas.indexOf(novaTarefa) !== -1) return;
+
+      const novasTarefas = [...tarefas];
+
+      if(index === -1) {
+        this.setState({
+          tarefas: [...novasTarefas, novaTarefa],
+          novaTarefa: ''
+        });
+      } else {
+        novasTarefas[index] = novaTarefa;
+
+        this.setState({
+          tarefas: [...novasTarefas],
+          index: -1
+        })
+      }
+
+
+    }
+
+    handleEdit = (e, index) => {
+      const { tarefas } = this.state;
+
+      this.setState({
+        index,
+        novaTarefa: tarefas[index]
+      })
+    }
+
+    handleDelete = (e, index) => {
+      const { tarefas } = this.state;
+      const novasTarefas = [...tarefas];
+
+      novasTarefas.splice(index, 1)
+
+      this.setState({
+        tarefas: [...novasTarefas],
+      })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            novaTarefa: e.target.value,
+        })
+    }
+
+    render() {
+        const { novaTarefa, tarefas } = this.state;
+
+        return (
+            <div className="main">
+                <h1>Lista de tarefas</h1>
+
+                <form onSubmit={this.handleSubmit} action="#" className="form">
+                    <input
+                        onChange={this.handleChange}
+                        type="text"
+                        value={novaTarefa}
+                    />
+                    <button type="submit">
+                        <FaPlus/>
+                    </button>
+                </form>
+
+                <ul className="tarefas">
+                  {
+                    tarefas.map((tarefa, index) => (
+                      <li key={tarefa}>
+                        { tarefa }
+                        <span>
+                          <FaEdit
+                            className="edit"
+                            onClick={(e) => this.handleEdit(e, index)}
+                          />
+                          <FaWindowClose
+                            onClick={(e) => this.handleDelete(e, index)}
+                            className="delete"
+                          />
+                        </span>
+                      </li>
+                    ))
+                  }
+                </ul>
+            </div>
+        )
+    }
+}
